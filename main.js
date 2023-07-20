001013d321fee66e3fd1ff9167a6499a35d84937
NicolasVdev <nicolasvadelorge.dev@gmail.com>
2023-07-20 14:28:50 +0200

    Observer

--- main.js
+++ main.js
@@ -13,16 +13,11 @@ searchMovieName.addEventListener("click", (e) => {
         `http://www.omdbapi.com/?s=${movie}&apikey=574be188`
       );
       const movieSearch = await response.json();
-      const containerDeVincent = document.body.appendChild(
+      const container = document.body.appendChild(
         document.createElement("div")
       );
-      containerDeVincent.setAttribute("id", "containerId");
-      containerDeVincent.classList.add(
-        "deletable",
-        "flex",
-        "flex-col",
-        "items-center"
-      );
+      container.setAttribute("id", "containerId");
+      container.classList.add("deletable", "flex", "flex-col", "items-center");
       for (let i = 0; i < movieSearch.Search.length; i++) {
         try {
           const movieId = await fetch(
@@ -32,12 +27,12 @@ searchMovieName.addEventListener("click", (e) => {
           const div = document
             .getElementById("containerId")
             .appendChild(document.createElement("div"));
-          div.classList.add("deletable");
+          div.classList.add("deletable", "card-visibility");
           const addMovieElement = (div, title, image, date, plot) => {
             div.innerHTML = `
             <div class="m-auto">
               <div class = "max-w-xl rounded-lg shadow-md lg:flex md:flex shadow-sky-600 mt-10 bg-white">
-                <img class="object-cover w-full md:w-1/2 lg:w-1/3" src="${image}" alt="image">
+                <img class="lazy-image object-cover w-full md:w-1/2 lg:w-1/3" src="${image}" alt="image">
                 <div class="px-6 py-4">
                   <h4 class="mb-3 text-base font-semibold tracking-tight text-sky-600">
                     ${title}
@@ -51,26 +46,33 @@ searchMovieName.addEventListener("click", (e) => {
                 </div>
               </div>
             </div>
-            <div id="myModal${i}" class="modal">
-              <div class="modal-content">
-              <span class="close">&times;</span>
-                          <div class="m-auto">
-              <div class = "max-w-xl rounded-lg shadow-md lg:flex md:flex shadow-sky-600 mt-10 bg-white">
-                <img class="object-cover w-full md:w-1/2 lg:w-1/3" src="${image}" alt="image">
-                <div class="px-6 py-4">
-                  <h4 class="mb-3 text-base font-semibold tracking-tight text-sky-600">
-                    ${title}
-                  </h4>
-                  <p class="mb-2 text-sm leading-normal text-justify text-sky-900">
-                    ${date}
-                  </p>
-                  <p>
-                    ${plot}
-                  </p>
-                </div>
-              </div>
-            </div>
             `;
+            const divModal = document
+              .getElementById("containerId")
+              .appendChild(document.createElement("div"));
+            divModal.classList.add("deletable", "modal");
+            divModal.setAttribute("id", `myModal${i}`);
+            divModal.innerHTML = `
+              <div class="modal-content flex">
+                <div class="m-auto">
+                  <div class = "max-w-xl rounded-lg lg:flex md:flex shadow-sky-600 mt-10 bg-white">
+                    <img class="object-cover" src="${image}" alt="image">
+                    <div class="px-6 py-4">
+                      <h4 class="mb-3 text-base font-semibold tracking-tight text-sky-600">
+                        ${title}
+                      </h4>
+                      <p class="mb-2 text-sm leading-normal text-justify text-sky-900">
+                        ${date}
+                      </p>
+                      <p>
+                        ${plot}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <span class="close">&times;</span>
+              </div>
+              `;
             const modal = document.getElementById(`myModal${i}`);
             const btn = document.getElementById(`myBtn${i}`);
             const span = document.getElementsByClassName("close")[i];
@@ -93,6 +95,21 @@ searchMovieName.addEventListener("click", (e) => {
             movieIdSearch.Released,
             movieIdSearch.Plot
           );
+
+          const movieCards = document.querySelectorAll(".card-visibility");
+          const observer = new IntersectionObserver(
+            (entries) => {
+              entries.forEach((entry) => {
+                entry.target.classList.toggle("show", entry.isIntersecting);
+              });
+            },
+            {
+              threshold: 0.5,
+            }
+          );
+          movieCards.forEach((movieCard) => {
+            observer.observe(movieCard);
+          });
         } catch (error) {
           console.error("Response error", error.message);
         }
